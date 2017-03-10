de29f1bffd0d4e226656682c9cb1d8414c8f1e51
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 19:15:53 +0200

    Create NotFoundException.java

---
+++ NotFoundException.java
@@ -0,0 +1,11 @@
+
+@SuppressWarnings("serial")
+public class NotFoundException extends Exception {
+
+	public NotFoundException() {
+	}
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+}
