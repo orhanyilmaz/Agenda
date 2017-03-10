d73b7068f65cac676000dbe677884465b5bf525f
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 19:16:08 +0200

    Create NotWorkHoursException.java

---
+++ NotWorkHoursException.java
@@ -0,0 +1,11 @@
+
+@SuppressWarnings("serial")
+public class NotWorkHoursException extends Exception {
+	
+	public NotWorkHoursException() {
+	}
+
+	public NotWorkHoursException(String message) {
+		super(message);
+	}
+}
