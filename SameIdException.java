0dcbffa955f22a75386385740e83c880f801afc7
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 19:16:36 +0200

    Create SameIdException.java

---
+++ SameIdException.java
@@ -0,0 +1,11 @@
+
+@SuppressWarnings("serial")
+public class SameIdException extends Exception {
+	
+	public SameIdException() {
+	}
+
+	public SameIdException(String message) {
+		super(message);
+	}
+}
