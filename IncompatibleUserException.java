afd8dd0ea7cafe09f14b8fcb8fc13c0579751478
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 19:15:00 +0200

    Create IncompatibleUserException.java

---
+++ IncompatibleUserException.java
@@ -0,0 +1,10 @@
+
+@SuppressWarnings("serial")
+public class IncompatibleUserException extends Exception {
+	public IncompatibleUserException() {
+	}
+
+	public IncompatibleUserException(String message) {
+		super(message);
+	}
+}
