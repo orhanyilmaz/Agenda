8449b44dd407496db77294749fb81f27675ad2e5
Orhan YILMAZ <orhan__yilmazz@hotmail.com>
2017-03-10 19:16:44 +0200

    Create Sport.java

---
+++ Sport.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class Sport implements Event {
+	private Calendar date;
+	private int id;
+	private String name;
+	private int quota;
+	private int duration;
+	ArrayList<Integer> persons = new ArrayList<Integer>();
+	private int workingTime = -1;
+
+	Sport(int id, Calendar date, String name, int quota,int workingTime ) {
+		this.id = id;
+		this.date = date;
+		this.name = name;
+		this.quota = quota;
+		duration = 4;
+		this.workingTime = workingTime;
+	}
+
+	public void setPersons(ArrayList<Integer> ids, int number) {
+		for (int i = 0; i < number; i++) {
+			persons.add(ids.get(i));
+		}		
+	}
+
+	public ArrayList<Integer> getPersons() {
+		return persons;
+	}
+
+	public int getWorkingTime() {
+		return workingTime;
+	}
+
+	public void setWorkingTime(int workingTime) {
+		this.workingTime = workingTime;
+	}
+
+	public Calendar getDate() {
+		return date;
+	}
+
+	public void setDate(Calendar date) {
+		this.date = date;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuota() {
+		return quota;
+	}
+
+	public void setQuota(int quota) {
+		this.quota = quota;
+	}
+
+}
